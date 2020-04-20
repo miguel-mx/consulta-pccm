4a1c6b88f2141bf5bbbffc468873c5b4f8720b5f
Miguel Angel Magaña <miguel@matmor.unam.mx>
2020-04-20 12:41:24 -0500

    Index

--- src/Controller/ConsultaController.php
+++ src/Controller/ConsultaController.php
@@ -20,9 +20,58 @@ class ConsultaController extends AbstractController
      */
     public function index(ConsultaRepository $consultaRepository): Response
     {
+        $consultas = $consultaRepository->findAll();
+        $contenido = array('yes' => 0, 'no' => 0);
+        $habilidades = array('ingenio' => 0, 'conocimientos' => 0, 'comprension' => 0);
+        $preguntas = array('calculos' => 0, 'demostraciones' => 0, 'opcion_multiple' => 0, 'trucos' => 0);
+        $temas = array(
+            'calculo' => 0,
+            'algebra_lineal' => 0,
+            'algebra_abstracta' => 0,
+            'combinatoria' => 0,
+            'probabilidad' => 0,
+            'teoria_numeros' => 0,
+            'ecuaciones' => 0,
+            'geometria_diferencial' => 0,
+            'topologia' => 0,
+            'fisica' => 0,
+        );
+
+        foreach ($consultas as $consulta) {
+            if($consulta->getContenido() == true) $contenido['yes']++;
+            else $contenido['no']++;
+
+            // Habilidades
+            if(in_array('Ingenio e inventiva', $consulta->getHabilidades())) $habilidades['ingenio']++;
+            if(in_array('Conocimientos', $consulta->getHabilidades())) $habilidades['conocimientos']++;
+            if(in_array('Comprensión de la teoría', $consulta->getHabilidades())) $habilidades['comprension']++;
+
+            // Preguntas
+            if(in_array('Cálculos rutinarios', $consulta->getPreguntas())) $preguntas['calculos']++;
+            if(in_array('Demostraciones sencillas', $consulta->getPreguntas())) $preguntas['demostraciones']++;
+            if(in_array('Opción múltiple', $consulta->getPreguntas())) $preguntas['opcion_multiple']++;
+            if(in_array('Trucos ingeniosos', $consulta->getPreguntas())) $preguntas['trucos']++;
+
+            // Temas
+            if(in_array('Calculo', $consulta->getTemas())) $temas['calculo']++;
+            if(in_array('Álgebra lineal', $consulta->getTemas())) $temas['algebra_lineal']++;
+            if(in_array('Álgebra abstracta', $consulta->getTemas())) $temas['algebra_abstracta']++;
+            if(in_array('Combinatoria', $consulta->getTemas())) $temas['combinatoria']++;
+            if(in_array('Probabilidad', $consulta->getTemas())) $temas['probabilidad']++;
+            if(in_array('Teoría de números', $consulta->getTemas())) $temas['teoria_numeros']++;
+            if(in_array('Ecuaciones diferenciales', $consulta->getTemas())) $temas['ecuaciones']++;
+            if(in_array('Geometría diferencial', $consulta->getTemas())) $temas['geometria_diferencial']++;
+            if(in_array('Topología', $consulta->getTemas())) $temas['topologia']++;
+            if(in_array('Física', $consulta->getTemas())) $temas['fisica']++;
+
+        }
 
         return $this->render('consulta/index.html.twig', [
-            'consultas' => $consultaRepository->findAll(),
+            'consultas' => $consultas,
+            'contenido' => $contenido,
+            'habilidades' => $habilidades,
+            'preguntas' => $preguntas,
+            'temas' => $temas,
         ]);
     }
 
